fb7b80f8e398f0911b831b1ab62ed9bc47f5daba
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-05-10 20:43:44 +0200

    swiper-slide

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-const barsBtn = document.querySelector(".fa-bars");
-// const navContent = document.querySelector(".navigation");
-// const timesBtn = document.querySelector(".fa-times");
-
-// barsBtn.addEventListener("click", function () {
-//   navContent.style.display = "flex";
-//   barsBtn.style.display = "none";
-//   timesBtn.style.zIndex = "2";
-// });
-
-// timesBtn.addEventListener("click", function () {
-//   navContent.style.display = "none";
-//   barsBtn.style.display = "block";
-//   timesBtn.style.zIndex = "-1";
-// });
-
-// const addNavigation = function () {
-//   this.classList.toggle("active");
-//   console.log("działa");
-// };
-
-// barsBtn.addEventListener("click", addNavigation());
+var swiper = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
