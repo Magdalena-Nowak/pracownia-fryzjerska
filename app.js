f7960ba400c113687289fce15eca655fa1e8241b
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-05-13 16:17:29 +0200

    add navigation

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const openNavBtn = document.querySelector(".fa-bars");
+const swiperContainer = document.querySelector(".swiper-container");
+const navigation = document.querySelector(".navigation");
+const closeNavBtn = document.querySelector(".fa-times");
 var swiper = new Swiper(".mySwiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
@@ -10,3 +14,15 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+openNavBtn.addEventListener("click", openingNavigation);
+closeNavBtn.addEventListener("click", closingNavigation);
+
+function openingNavigation() {
+  navigation.classList.add("active");
+}
+
+function closingNavigation() {
+  navigation.classList.remove("active");
+  console.log("bla bla");
+}
