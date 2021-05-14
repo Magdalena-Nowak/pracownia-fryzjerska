110e5f63067f7b938b1124778584d27abb5b9ec2
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-05-14 16:36:08 +0200

    add pricelist

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const openNavBtn = document.querySelector(".fa-bars");
 const swiperContainer = document.querySelector(".swiper-container");
 const navigation = document.querySelector(".navigation");
 const closeNavBtn = document.querySelector(".fa-times");
+const openPriceBtns = document.querySelectorAll(".body--uncover");
+const hidePriceBtns = document.querySelectorAll(".body--hide");
+const cardsContainers = document.querySelectorAll(".container");
+const priceCards = document.querySelectorAll(".priceList__card");
 
 var swiper = new Swiper(".mySwiper", {
   scrollbar: {
@@ -17,7 +21,20 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 openNavBtn.addEventListener("click", openingNavigation);
+
 closeNavBtn.addEventListener("click", closingNavigation);
+
+openPriceBtns.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    openingPrices(index);
+  });
+});
+
+hidePriceBtns.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    closingPrices(index);
+  });
+});
 
 function openingNavigation() {
   navigation.classList.add("active");
@@ -25,4 +42,20 @@ function openingNavigation() {
 
 function closingNavigation() {
   navigation.classList.remove("active");
+}
+
+function openingPrices(index) {
+  cardsContainers[index].classList.add("open");
+  cardsContainers[index].classList.add("conditionalOpen");
+  cardsContainers[index].classList.remove("conditionalClose");
+  openPriceBtns[index].style.display = "none";
+  hidePriceBtns[index].style.display = "block";
+}
+
+function closingPrices(index) {
+  cardsContainers[index].classList.remove("open");
+  cardsContainers[index].classList.remove("conditionalOpen");
+  cardsContainers[index].classList.add("conditionalClose");
+  openPriceBtns[index].style.display = "block";
+  hidePriceBtns[index].style.display = "none";
 }
