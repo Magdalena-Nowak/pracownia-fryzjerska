0017ee278934720b7d00b8631b6804b5fe947296
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-07-09 12:49:16 +0200

    change galley code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
-// NAVIGATION
-//Zmienne
+//VARIABLES
 const navigation = document.querySelector(".navigation__wrapper");
 const navItems = document.querySelectorAll(".navigation__item");
 const sections = document.querySelectorAll("#home section");
@@ -9,8 +8,39 @@ const openNavBtn = document.querySelector(".navigation__open-btn");
 const closeNavBtn = document.querySelector(".navigation__close-btn");
 const menuNav = document.querySelector("#home");
 const logoNav = document.querySelector(".navigation__link");
+const descriptionWrapper = document.querySelector(
+  ".about__description-wrapper"
+);
+const photoWrapper = document.querySelector(".about__photo-wrapper");
+const openPriceBtns = document.querySelectorAll(".price-list__reveal");
+const hidePriceBtns = document.querySelectorAll(".price-list__hide");
+const cardsContainers = document.querySelectorAll(".price-list__container");
+const priceSection = document.querySelector("#prices");
+const galleryContent = document.querySelector(".gallery__wrapper");
+const gallery = document.querySelector("#gallery");
+const gallerySlider = document.querySelector(".gallery__swiper");
+const smallImages = document.querySelectorAll(".gallery__thumb");
+const regularImages = document.getElementsByClassName("gallery__regular-image");
+const regularImagesArray = [];
+const closeBigGallery = document.querySelector(".gallery__close-icon");
+const bigImage = document.querySelector(".gallery__big-image");
+const mainContent = document.querySelector("main");
+const footerContent = document.querySelector("footer");
+const headerContent = document.querySelector("header");
+const rightBtn = document.querySelector(".gallery__right-icon");
+const leftBtn = document.querySelector(".gallery__left-icon");
+const sectionAbout = document.querySelector(".about");
+const allImages = regularImages.length - 1;
+let imageNr;
+let currentImageNr = 6;
+let more = 0;
+const swipeImage = document.querySelector(".swiper__image");
+const swipeSlides = [...document.querySelectorAll(".swiper__slide")];
 
-//Implementacja
+const time = 3000;
+let active = 0;
+
+// NAVIGATION
 openNavBtn.addEventListener("click", openingNavigation);
 closeNavBtn.addEventListener("click", closingNavigation);
 
@@ -43,23 +73,13 @@ logoNav.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
 //SLIDER GALLERY
-
-//Zmienne
-
 const swipeList = [
   { img: "./img/image1-large.png" },
   { img: "./img/image2-large.png" },
   { img: "./img/image3-large.png" },
 ];
-const swipeImage = document.querySelector(".swiper__image");
-const swipeSlides = [...document.querySelectorAll(".swiper__slide")];
-//Interfejs
-
-const time = 3000;
-let active = 0;
-
-//Implementacje
 
 const autoChangeSlides = () => {
   const activeSlide = swipeSlides.findIndex((slide) =>
@@ -79,7 +99,6 @@ const changeSwipe = () => {
 };
 
 let timeInterval = setInterval(changeSwipe, time);
-const swiperControllers = document.querySelectorAll(".swiper i");
 
 const changeSlides = (e) => {
   if (
@@ -102,26 +121,19 @@ const changeSlides = (e) => {
 window.addEventListener("click", changeSlides);
 
 changeSwipe();
-// About
 
-const descriptionWrapper = document.querySelector(
-  ".about__description-wrapper"
-);
-const photoWrapper = document.querySelector(".about__photo-wrapper");
+//ABOUT
 ScrollTrigger.matchMedia({
-  //small
   "(max-width: 991px)": function () {
     gsap.fromTo(
       descriptionWrapper,
-      3,
-      { opacity: 0, y: "100%" },
+      { opacity: 0, x: "-100%" },
       {
         opacity: 1,
-        y: "0%",
+        x: "0%",
         scrollTrigger: {
-          trigger: ".about",
-          start: "top 40%",
-          markers: true,
+          trigger: sectionAbout,
+          start: "top 50%",
         },
         duration: 3,
       }
@@ -129,21 +141,18 @@ ScrollTrigger.matchMedia({
 
     gsap.fromTo(
       photoWrapper,
-      4,
-      { x: "100%"},
+      { x: "100%" },
       {
         x: "0%",
         scrollTrigger: {
           trigger: photoWrapper,
-          start: "top 40%",
-          markers: true,
+          start: "top 50%",
         },
-        duration: 3,
+        duration: 6,
       },
       "-=3"
     );
   },
-  // large
   "(min-width: 992px)": function () {
     gsap.fromTo(
       descriptionWrapper,
@@ -152,153 +161,132 @@ ScrollTrigger.matchMedia({
       {
         opacity: 1,
         scrollTrigger: {
-          trigger: descriptionWrapper,
-          start: "top 20%",
-          markers: true,
+          trigger: ".about",
+          start: "top 40%",
         },
         duration: 3,
       }
     );
-
     gsap.fromTo(
       photoWrapper,
       4,
-      { x: "100%"},
+      { x: "100%" },
       {
         x: "0%",
         scrollTrigger: {
-          trigger: descriptionWrapper,
-          start: "top 20%",
-          markers: true,
+          trigger: ".about",
+          start: "top 40%",
         },
         duration: 3,
       },
       "-=1"
     );
   },
-
-  all: function () {
-    // ScrollTriggers created here aren't associated with a particular media query,
-    // so they persist.
+  "(max-width: 1199px)": function () {
+    openPriceBtns.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        gsap.fromTo(
+          cardsContainers[index],
+          1,
+          { display: "none" },
+          { display: "flex" }
+        );
+        const cards =
+          cardsContainers[index].querySelectorAll(".price-list__body");
+        gsap.fromTo(cards, 2, { x: "-150%" }, { x: "0%", ease: "back" });
+        openPriceBtns[index].style.display = "none";
+        hidePriceBtns[index].style.display = "flex";
+      });
+    });
+    hidePriceBtns.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        const cards =
+          cardsContainers[index].querySelectorAll(".price-list__body");
+        gsap.fromTo(cards, 2, { x: "0%" }, { x: "-150%" });
+        gsap.fromTo(
+          cardsContainers[index],
+          1,
+          { display: "flex" },
+          { display: "none", delay: 0.8 }
+        );
+        openPriceBtns[index].style.display = "flex";
+        hidePriceBtns[index].style.display = "none";
+        priceSection.scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
   },
 });
-// Prices
-// Zmienne
-const openPriceBtns = document.querySelectorAll(".price-list__reveal");
-const hidePriceBtns = document.querySelectorAll(".price-list__hide");
-const cardsContainers = document.querySelectorAll(".price-list__container");
-const priceCards = document.querySelectorAll(".price-list__card");
-const priceSection = document.querySelector("#prices");
-// console.log(priceHeaders);
 
-//Implementacja
-openPriceBtns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    cardsContainers[index].classList.add("open");
-    cardsContainers[index].classList.add("opening");
-    openPriceBtns[index].style.display = "none";
-    hidePriceBtns[index].style.display = "flex";
-  });
-});
+//GALLERY
 
-hidePriceBtns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    cardsContainers[index].classList.remove("open");
-    openPriceBtns[index].style.display = "flex";
-    hidePriceBtns[index].style.display = "none";
-    priceSection.scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
-
-//Gallery
-
-// Zmienne
-const moreBtn = document.querySelector(".gallery__more");
-const galleryContent = document.querySelector(".gallery__wrapper");
-
-//Big image implementation
-const gallerySlider = document.querySelector(".gallery__swiper");
-const smallImages = document.querySelectorAll(".gallery__thumb");
-const regularImages = document.getElementsByClassName(
-  "gallery__regular-image "
-);
-const closeRegularGallery = document.querySelector(".gallery_close-icon");
-const bigImage = document.querySelector(".gallery__big-wrapper");
-const mainContent = document.querySelector("main");
-const footerContent = document.querySelector("footer");
-const headerContent = document.querySelector("header");
-const rightBtn = document.querySelector(".gallery__right-icon");
-const leftBtn = document.querySelector(".gallery__left-icon");
-const allImages = regularImages.length - 1;
-let imageNr;
-
-//Implementacja
-moreBtn.addEventListener("click", addElements);
-let currentImageNr = 13;
-function addElements() {
-  let allButtons = document.querySelectorAll(".gallery__more");
-  if (currentImageNr > allImages) return;
-  let currentMax = currentImageNr + 12;
-  for (let i = currentImageNr; i < currentMax; i++) {
-    if (i > allImages) {
-      break;
-    }
-    const imageDiv = document.createElement("div");
-    imageDiv.classList.add("gallery__regular-wrapper");
-    galleryContent.appendChild(imageDiv);
-    imageDiv.innerHTML = `<img class="gallery__regular-image" src="./img//img${i}.jpg" alt="Zdjęcie fryzury">`;
-    allButtons.forEach((btn) => {
-      btn.style.display = "none";
-    });
+function galleryExtend() {
+  for (let i = 0; i < 51; i++) {
+    const div = document.createElement("div");
+    div.classList.add("gallery__regular-wrapper");
+    let parent = gallery.querySelector(".gallery__wrapper");
+    let parentLastChild = parent.lastElementChild;
+    parent.insertBefore(div, parentLastChild);
+    galleryContent.appendChild(div);
+    gsap.fromTo(
+      div,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: div,
+          start: "top 50%",
+          end: "-10 top",
+        },
+      }
+    );
+    const newImage = document.createElement("img");
+    const imageSrc = newImage.setAttribute("src", `./img//img${i + 1}t.jpg`);
+    newImage.classList.add("gallery__regular-image");
+    div.appendChild(newImage);
+    more += 1;
+    regularImagesArray.push(div);
+    // }
     clickableRegularImages();
-  }
-  currentImageNr = currentMax;
-  console.log(currentImageNr);
-  if (currentImageNr === allImages + 1) {
-    const btnMore = document.createElement("div");
-    btnMore.classList.add("gallery__more");
-    const textMore = document.createElement("p");
-    textMore.classList.add("gallery__more-txt");
-    btnMore.appendChild(textMore);
-    textMore.innerText = "Zapraszam do kontaktu";
-    galleryContent.appendChild(btnMore);
-  } else {
-    const btnMore = document.createElement("div");
-    btnMore.classList.add("gallery__more");
-    const textMore = document.createElement("p");
-    textMore.classList.add("gallery__more-txt");
-    btnMore.appendChild(textMore);
-    textMore.innerText = "Więcej";
-    galleryContent.appendChild(btnMore);
-    btnMore.addEventListener("click", addElements);
   }
 }
 
-// closeRegularGallery.addEventListener("click", function () {
-//   gallerySlider.classList.remove("active");
-//   mainContent.style.display = "block";
-//   footerContent.style.display = "flex";
-//   headerContent.style.display = "block";
-//   galleryContent.scrollIntoView({
-//     behavior: "smooth",
-//   });
-// });
-// function clickableRegularImages() {
-//   regularImages.forEach((img, index) => {
-//     img.addEventListener("click", () => {
-//       imageNr = index;
-//       gallerySlider.classList.add("active");
-//       mainContent.style.display = "none";
-//       footerContent.style.display = "none";
-//       headerContent.style.display = "none";
-//       bigImage.setAttribute("src", `./img/img${imageNr + 1}.jpg`);
-//       dynamicChangeImages();
-//       handlingArrows();
-//     });
-//   });
-// }
+galleryExtend();
+
+//Small implementation
+
+closeBigGallery.addEventListener("click", function () {
+  gallerySlider.classList.remove("active");
+  gallerySlider.style.display = "none";
+  mainContent.style.display = "block";
+  footerContent.style.display = "flex";
+  headerContent.style.display = "block";
+  galleryContent.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+function clickableRegularImages() {
+  regularImagesArray.forEach((img, index) => {
+    img.addEventListener("click", () => {
+      imageNr = index;
+      gallerySlider.classList.add("active");
+      gallerySlider.style.display = "flex";
+      // gallerySlider.style.zIndex = 100;
+      mainContent.style.display = "none";
+      footerContent.style.display = "none";
+      headerContent.style.display = "none";
+      bigImage.setAttribute("src", `./img/img${imageNr + 1}t.jpg`);
+      dynamicChangeImages();
+      handlingArrows();
+    });
+  });
+}
+
+//console.log(regularImagesArray);
+
 function dynamicChangeImages() {
   smallImages.forEach((image, index) => {
     image.addEventListener("click", function () {
@@ -317,6 +305,7 @@ function handlingArrows() {
       imageNr++;
       bigImage.setAttribute("src", `./img/img${imageNr + 1}.jpg`);
     }
+    changingPhotoOrder();
   });
   leftBtn.addEventListener("click", function () {
     if (imageNr <= 0) {
@@ -326,6 +315,14 @@ function handlingArrows() {
       imageNr--;
       bigImage.setAttribute("src", `./img/img${imageNr + 1}.jpg`);
     }
+    changingPhotoOrder();
   });
 }
-// clickableRegularImages();
+
+function changingPhotoOrder(imageNr) {
+  if(imageNr) {
+  imageOrder = 25;
+  bigImage.style.order = imageOrder;
+  console.log(imageOrder);
+  }
+}
